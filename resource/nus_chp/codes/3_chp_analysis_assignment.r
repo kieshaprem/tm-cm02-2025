bbb0132f2a54ba31be09af9473254686fc342b43
Kiesha Prem <12025079+kieshaprem@users.noreply.github.com>
2025-02-13 07:31:38 +0700

    updates

---
+++ resource/nus_chp/codes/3_chp_analysis_assignment.r
@@ -0,0 +1,92 @@
+############# Homework ############# 
+
+# import the data for analysis
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# ++++++++ Question 1 ++++++++ #
+
+# 1.	Populate the table 
+
+## a) Age and CVD (Tips:  use the sd() function to calculate standard deviation, e.g., sd(chp$age[chp$cvd %in% 1]))
+
+
+
+## b) BMI and CVD 
+
+
+
+
+## c) LDL cholesterol and CVD 
+
+
+
+## d) Gender and CVD 
+
+
+
+## e) Ethnicity and CVD 
+
+
+
+## f) Smoking status and CVD 
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# ++++++++ Question 2 ++++++++ #
+
+# 2.	Is BMI (being overweight) associated with CVD? 
+# Populate your results in the table.
+
+
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# ++++++++ Question 3 ++++++++ #
+
+# 3.	Is smoking associated with BMI (being overweight)? 
+# Populate your results in the table.
+
+
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# ++++++++ Question 4 ++++++++ #
+
+# 4.	Is smoking associated with CVD? 
+# Populate your results in the table.
+
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# ++++++++ Question 5 ++++++++ #
+
+# 5. Is there a linear association between BMI and LDL cholesterol levels? 
+# Report the correlation coefficient and p value of correlation. 
+
+
+
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
